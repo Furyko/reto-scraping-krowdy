6da2aca02606e86ad09de9577ef3ca2826338692
Julian Palavecino <samuguayfenix@gmail.com>
2022-08-12 20:20:28 -0300

    Scrapper refactored.

--- src/scripts/scrapper.js
+++ src/scripts/scrapper.js
@@ -1,11 +1,24 @@
 import { $,$$ } from '../utils/selectors.js'
 
-const resultsContainer = $$('.reusable-search__entity-result-list > .reusable-search__result-container')
 
-resultsContainer.forEach((resultItem) => {
-    const profileUrlItem = $(
-        '.entity-result > .entity-result__item > .entity-result__content > .mb1 > div > div > span > span > a',
-        resultItem
-        )
-    console.log(profileUrlItem.attributes.href.textContent)
-})
+async function startScraping() {
+    function getProfiles() {
+        const resultsContainer = $$('.reusable-search__entity-result-list > .reusable-search__result-container')
+        const profileUrlList = []
+        resultsContainer.forEach((resultItem) => {
+            const profileUrlItem = $(
+                '.entity-result > .entity-result__item > .entity-result__content > .mb1 > div > div > span > span > a',
+                resultItem
+            )
+            try {
+                console.log(profileUrlItem.attributes.href.textContent)
+                profileUrlList.push(profileUrlItem.attributes.href.textContent)
+            } catch {}
+        })
+        return profileUrlList
+    }
+
+    getProfiles()
+}
+
+startScraping()
