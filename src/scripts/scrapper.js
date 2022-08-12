189f1bf962e7708500642cc3617fd18234f3a62e
Julian Palavecino <samuguayfenix@gmail.com>
2022-08-12 02:21:04 -0300

    Edited scrapper to get url from each profile in the list.

--- src/scripts/scrapper.js
+++ src/scripts/scrapper.js
@@ -1,1 +1,11 @@
-console.log("This is the scrapper")
+import { $,$$ } from '../utils/selectors.js'
+
+const resultsContainer = $$('.reusable-search__entity-result-list > .reusable-search__result-container')
+
+resultsContainer.forEach((resultItem) => {
+    const profileUrlItem = $(
+        '.entity-result > .entity-result__item > .entity-result__content > .mb1 > div > div > span > span > a',
+        resultItem
+        )
+    console.log(profileUrlItem.attributes.href.textContent)
+})
