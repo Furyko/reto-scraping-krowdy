67d6b050e41228ee76a6322ef2460add3e58de60
Julian Palavecino <samuguayfenix@gmail.com>
2022-08-12 20:22:55 -0300

    Added function to get info from a single profile

--- src/scripts/scrapper.js
+++ src/scripts/scrapper.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { $,$$ } from '../utils/selectors.js'
 
 
@@ -16,6 +17,42 @@ async function startScraping() {
             } catch {}
         })
         return profileUrlList
+    }
+
+    async function getProfileInfo() {
+        const token = document.cookie
+            .split(';')
+            .find(cookie => cookie.includes('JSESSIONID'))
+            .replace(/JSESSIONID=|"/g, '').trim()
+
+        const profileUrlName = window.location.href.match(/in\/.+\//g)[0].slice(3, -1)
+        const { data } = await axios.get(
+            `https://www.linkedin.com/voyager/api/identity/profiles/${profileUrlName}/profileContactInfo`,
+            {
+                headers: { 'csrf-token': token }
+            }
+        )
+
+        const experienceElements = $$('#experience ~ .pvs-list__outer-container > ul > li')
+        const experienceTitles = []
+        experienceElements.forEach((experienceListItem) => {
+            const experienceTitleElement = $('span[aria-hidden]', experienceListItem)
+            experienceTitles.push(experienceTitleElement.textContent)
+        })
+
+        const educationElements = $$('#education ~ .pvs-list__outer-container > ul > li')
+        const educationTitles = []
+        educationElements.forEach((educationLitItem) => {
+            const educationTitleElement = $('span[aria-hidden]', educationLitItem)
+            educationTitles.push(educationTitleElement.textContent)
+        })
+
+        const profile = {
+            contactInfo: await data,
+            experienceTitles,
+            educationTitles
+        }
+        console.log(profile)
     }
 
     getProfiles()
