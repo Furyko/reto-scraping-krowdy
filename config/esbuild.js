16d8b8a731da19b0440604868a80bcec10a4f6f3
Julian Palavecino <samuguayfenix@gmail.com>
2022-08-12 01:42:25 -0300

    Added esbuild. Added Service Worker. Added new config to package.json.

---
+++ config/esbuild.js
@@ -0,0 +1,12 @@
+import esbuild from 'esbuild'
+
+esbuild.build({
+    entryPoints: ['src/sw.js'],
+    watch: true,
+    bundle: true,
+    outdir: 'dist',
+    minify: true,
+    allowOverwrite: true
+})
+    .then(response => console.log(JSON.stringify(response)))
+    .catch(err => console.log(err))
